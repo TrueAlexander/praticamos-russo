d83ff106207b395e46c3489526c2662a7bbb8392
Alexander <caminante.msk@gmail.com>
2023-08-18 15:01:00 -0300

    layout test

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,8 +23,8 @@ export default function RootLayout({children}) {
       <body className="bg-[#2b2737] p-4 h-screen absolute left-0 right-0 top-0 bottom-0 overflow-hidden box-border flex flex-col justify-center ">
         <AuthProvider>
           <div className="w-fit mx-auto">
-            <main className={`${quicksand.variable} font-quicksand max-w-[900px] w-full m-auto flex flex-col items-center justify-center h-screen overflow-hidden relative`}>         
-              <Link href='/' className="max-h-[90px] mt-[65px] absolute top-0">
+            <main className={`${quicksand.variable} font-quicksand max-w-[900px] w-full h-full m-auto flex flex-col items-center justify-center h-screen overflow-hidden relative`}>         
+              <Link href='/' className="max-h-[90px] mt-[65px] fixed top-0">
                 <div className="w-[90px] h-full cursor-pointer rounded-[50%] bg-gradient-radial from-[#dcddd8] to-[#9f50ac] border-4 border-[#9f50ac]">
                   <Image 
                     className="" 
@@ -33,10 +33,10 @@ export default function RootLayout({children}) {
                   />
                 </div>     
               </Link>
-              <div className="pt-[30px]">
+              <div className="pt-[30px] bg-blue-500">
                 {children} 
               </div>                   
-              <p className='absolute bottom-0 text-white my-2 p-4 text-[10px]'>
+              <p className='fixed bottom-0 text-white my-2 p-4 text-[10px] bg-yellow-500'>
                 Criado por <a href="https://www.eformaliza.com/" target="_blank" >e-Formaliza</a>
               </p>
             </main>  
