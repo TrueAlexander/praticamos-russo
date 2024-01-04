e229809ddbbb6aecca6065faaf586adf1747b2ff
Alexander <caminante.msk@gmail.com>
2024-01-04 12:09:14 -0300

    test using inline styles

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -29,10 +29,13 @@ export const metadata = {
 export default function RootLayout({children}) { 
   return (
     <html lang="en">
-      <body className="bg-[#2b2737] p-4 box-border flex flex-col h-screen absolute top-0 bottom-0 left-0 right-0 justify-center overflow-hidden bodyClass">
+      <body 
+        style={{ height: '100vh', position: "absolute", top: 0, left: 0, right: 0  }} 
+        className="bg-[#2b2737] p-4 box-border flex flex-col h-screen absolute top-0 bottom-0 left-0 right-0 justify-center overflow-hidden bodyClass"
+      >
         <AuthProvider>
           <div className="w-fit mx-auto">
-            <main className={`${quicksand.variable} font-quicksand max-w-[900px] w-full m-auto flex flex-col items-center justify-center overflow-hidden relative main`}>         
+            <main className={`${quicksand.variable} font-quicksand max-w-[900px] w-full m-auto flex flex-col items-center h-screen justify-center overflow-hidden relative main`}>         
               <Link href='/' className="max-h-[90px] mt-[65px] fixed top-0 logo">
                 <div className="w-[90px] h-full cursor-pointer rounded-[50%] bg-gradient-radial from-[#dcddd8] to-[#9f50ac] border-4 border-[#9f50ac]">
                   <Image 
