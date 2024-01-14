78fb152d5048cc6a5dd3ecf7653c2cde5d8fbb2c
Alexander <caminante.msk@gmail.com>
2024-01-14 14:48:53 -0300

    test loading

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -42,7 +42,8 @@ export default function Home() {
   return (
     <div className='text-center flex flex-col justify-center'>
       {isLoading 
-        ? <Loading/>
+        // ? <Loading/>
+        ? ""
         : <>
             <ButtonAuth setShowModal={setShowModal} name={name} signOut={signOut} nameShow={nameShow} setIsLoading={setIsLoading}/>
             <div className='bg-green-300 var-content-class'>
