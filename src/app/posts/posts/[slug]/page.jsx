45a1c3f8c03d37fcea7f8570b1b675945c94d190
Alexander <caminante.msk@gmail.com>
2025-08-10 20:08:08 -0300

    to test blogs layout

--- src/app/posts/posts/[slug]/page.jsx
+++ src/app/posts/posts/[slug]/page.jsx
@@ -1,83 +1,83 @@
 import styles from "./singlePage.module.css"
 import Image from "next/image"
-import { formatDate } from "@/utils/dateFormat"
-import CatTitle from "@/components/blog/filterTitle/FilterTitle"
+// import { formatDate } from "@/utils/dateFormat"
+// import CatTitle from "@/components/blog/filterTitle/FilterTitle"
 import Reactions from "@/components/blog/reactions/Reactions"
 import Link from "next/link"
 import Tags from "@/components/blog/tags/Tags"
-import ManagePostButtons from "@/components/blog/managePostButtons/ManagePostButtons"
-import Comments from "@/components/blog/comments/Comments"
-import CommentsInteractive from "@/components/blog/commentsInteractive/CommentsInteractive"
-import { stripHtmlTags } from "@/utils/stripHtmlTags"
-import { convertYouTubeLinksToEmbed } from "@/utils/convertYouTubeLinksToEmbed"
-import ReportButton from "@/components/globals/reportButton/ReportButton"
+// import ManagePostButtons from "@/components/blog/managePostButtons/ManagePostButtons"
+// import Comments from "@/components/blog/comments/Comments"
+// import CommentsInteractive from "@/components/blog/commentsInteractive/CommentsInteractive"
+// import { stripHtmlTags } from "@/utils/stripHtmlTags"
+// import { convertYouTubeLinksToEmbed } from "@/utils/convertYouTubeLinksToEmbed"
+// import ReportButton from "@/components/globals/reportButton/ReportButton"
 
-export async function generateMetadata({params}) {
-  const { slug } = params
+// export async function generateMetadata({params}) {
+//   const { slug } = params
 
-  const res = await fetch(`${process.env.BASE_URL}/api/get-one-post`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ slug }),
-    cache: "no-store", // Ensures the data is not cached and is always fetched fresh
-  })
-  if (!res.ok) {
-    const errorText = await res.text(); // read as text to inspect
-    console.error("Error fetching post:", errorText)
-    throw new Error("Failed to fetch post data")
-  }
-  const data = await res.json()
+//   const res = await fetch(`${process.env.BASE_URL}/api/get-one-post`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ slug }),
+//     cache: "no-store", // Ensures the data is not cached and is always fetched fresh
+//   })
+//   if (!res.ok) {
+//     const errorText = await res.text(); // read as text to inspect
+//     console.error("Error fetching post:", errorText)
+//     throw new Error("Failed to fetch post data")
+//   }
+//   const data = await res.json()
 
-  const optimizedImgUrl = data.res.img?.replace('/upload/', '/upload/q_auto,f_jpg/')
+//   const optimizedImgUrl = data.res.img?.replace('/upload/', '/upload/q_auto,f_jpg/')
   
-  return {
-    title: data.res.title,
-    description:  data.res.metaDescription || data.res.introduction || stripHtmlTags(data.res.content),
-    alternates: {
-      canonical: `${process.env.BASE_URL}/posts/${slug}`,
-    },
-    openGraph: {
-      title: data.res.title,
-      description: data.res.introduction || data.res.metaDescription || stripHtmlTags(data.res.content),
-      type: "article",
-      url: `${process.env.BASE_URL}/posts/${slug}`,
-      siteName: "MotoPost",
-      images: [
-        {
-          url: optimizedImgUrl || "https://motopost.com.br/opengraph-image.jpg", 
-          width: 1200,
-          height: 630,
-          alt: data.res.title,
-          type: "image/jpeg",
-        }
-      ]
-    }
-  }
+//   return {
+//     title: data.res.title,
+//     description:  data.res.metaDescription || data.res.introduction || stripHtmlTags(data.res.content),
+//     alternates: {
+//       canonical: `${process.env.BASE_URL}/posts/${slug}`,
+//     },
+//     openGraph: {
+//       title: data.res.title,
+//       description: data.res.introduction || data.res.metaDescription || stripHtmlTags(data.res.content),
+//       type: "article",
+//       url: `${process.env.BASE_URL}/posts/${slug}`,
+//       siteName: "MotoPost",
+//       images: [
+//         {
+//           url: optimizedImgUrl || "https://motopost.com.br/opengraph-image.jpg", 
+//           width: 1200,
+//           height: 630,
+//           alt: data.res.title,
+//           type: "image/jpeg",
+//         }
+//       ]
+//     }
+//   }
   
-}
+// }
 
-const getPostBySlug = async (slug) => {
-  try {
-    const res = await fetch(`${process.env.BASE_URL}/api/get-one-post`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ slug }),
-      cache: "no-store", // Ensures the data is not cached and is always fetched fresh
-    })
-    const data = await res.json()
-    return data.res
-  } catch (error) {
-    console.log(error)
-  }
-}
+// const getPostBySlug = async (slug) => {
+//   try {
+//     const res = await fetch(`${process.env.BASE_URL}/api/get-one-post`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ slug }),
+//       cache: "no-store", // Ensures the data is not cached and is always fetched fresh
+//     })
+//     const data = await res.json()
+//     return data.res
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
 export default async function SinglePostPage({ params }) {
   const { slug } = params
-  const data = await getPostBySlug(slug)
+  // const data = await getPostBySlug(slug)
 
   if (!data) {
     return <div>Post not found!</div> // You can show an error message if no data is returned
@@ -85,19 +85,19 @@ export default async function SinglePostPage({ params }) {
 
   return (
     <div className={styles.container}>
-      <Link href={`/blog/?catSlug=${data?.catSlug}`}>
+      {/* <Link href={`/blog/?catSlug=${data?.catSlug}`}>
         <CatTitle cat={data?.category} catSlug={data?.catSlug}/> 
-      </Link>
+      </Link> */}
       <div className={styles.detailsContainer}>
         <Reactions id={data?._id} />
-        {data?.author === "Deletado" ? (
+        {/* {data?.author === "Deletado" ? (
           <span className={styles.username}>{data.author}</span>
         ) : (
           <Link href={`/blog/?author=${data.author}`}>
             <span className={styles.username}>{data.author}</span>
           </Link>
-        )}
-        <span className={styles.date}>{formatDate(data?.createdAt)}</span>
+        )} */}
+        {/* <span className={styles.date}>{formatDate(data?.createdAt)}</span> */}
       </div>
       <h2 className={styles.title}>{data?.title}</h2>
       <div className={styles.infoContainer}>
@@ -116,19 +116,19 @@ export default async function SinglePostPage({ params }) {
         <div className={styles.contentContainer}>
           <div 
             className={styles.content}
-            dangerouslySetInnerHTML={{ __html: convertYouTubeLinksToEmbed(data?.content) }}
+            dangerouslySetInnerHTML={{ __html: data?.content }}
           />
           <div className={styles.tagsContainer}>
             <Tags tags={data?.tags} />
           </div>
-          <ManagePostButtons author={data?.author} slug={data?.slug} imagePublicId={data?.img} postId={data?._id} />
-          <ReportButton postId={data._id} title={data.title} email={data.authorEmail} name={data.author} />
+          {/* <ManagePostButtons author={data?.author} slug={data?.slug} imagePublicId={data?.img} postId={data?._id} />
+          <ReportButton postId={data._id} title={data.title} email={data.authorEmail} name={data.author} /> */}
           <div className={styles.comment}>
           <h4 className={styles.titleComment}>Comentários:</h4>
-            <CommentsInteractive postId={data._id}/>
+            {/* <CommentsInteractive postId={data._id}/>
             <div className="static-comments">
               <Comments postId={data._id}/>
-            </div> 
+            </div>  */}
           </div>      
         </div>
       </div>
--- src/components/blog/writeComment/WriteComment.jsx
+++ src/components/blog/writeComment/WriteComment.jsx
@@ -1,75 +1,75 @@
-"use client"
-import { useState } from "react"
-import styles from "./writeComment.module.css"
-import { ThemeContext } from "@/context/ThemeContext"
-import { useContext } from 'react'
-import { confirmAlert } from 'react-confirm-alert'
-import '@/utils/react-confirm-alert.css'
-import confirmAlertStyles from '@/utils/confirmAlert.module.css'
-import Loading from "@/app/loading"
-import { moderateText } from "@/utils/moderateText"
+// "use client"
+// import { useState } from "react"
+// import styles from "./writeComment.module.css"
+// import { ThemeContext } from "@/context/ThemeContext"
+// import { useContext } from 'react'
+// import { confirmAlert } from 'react-confirm-alert'
+// import '@/utils/react-confirm-alert.css'
+// import confirmAlertStyles from '@/utils/confirmAlert.module.css'
+// import Loading from "@/app/loading"
+// import { moderateText } from "@/utils/moderateText"
 
-export default function WriteComment({ handleSubmit, author, authorEmail, postId, parentId}) {
+// export default function WriteComment({ handleSubmit, author, authorEmail, postId, parentId}) {
 
-  const {theme} = useContext(ThemeContext)
-  const themeClass = theme === 'dark' ? confirmAlertStyles.darkConfirmAlert : confirmAlertStyles.lightConfirmAlert
-  const [isLoading, setIsLoading] = useState(false)
+//   const {theme} = useContext(ThemeContext)
+//   const themeClass = theme === 'dark' ? confirmAlertStyles.darkConfirmAlert : confirmAlertStyles.lightConfirmAlert
+//   const [isLoading, setIsLoading] = useState(false)
 
-  const [text, setText] = useState("")
-  const maxLength = 250
+//   const [text, setText] = useState("")
+//   const maxLength = 250
 
-  const handleNewComment = async (e) => {
-    e.preventDefault()
-    if (!text.trim()) return
-    setIsLoading(true)
-    if(text.length < 3 || text.length > maxLength) {
-      confirmAlert({
-        customUI: ({ onClose }) => (
-          <div className={themeClass}>
-            <p>Seu comentário deve conter entre 3 e 250 caracteres!</p>
-            <button 
-              className="button"
-              onClick={() => { onClose(); setIsLoading(false) }}
-            >
-              Ok
-            </button>
-          </div>
-        ),
-      })
-    } else {
-      const commentData = { author: author, text: moderateText(text.charAt(0).toUpperCase() + text.slice(1)), authorEmail }
-      // await handleSubmit({ author: author, text: text.charAt(0).toUpperCase() + text.slice(1), authorEmail })
-      await handleSubmit(commentData, postId, parentId)
-      setText()
-      setIsLoading(false)
-    }
-  }
+//   const handleNewComment = async (e) => {
+//     e.preventDefault()
+//     if (!text.trim()) return
+//     setIsLoading(true)
+//     if(text.length < 3 || text.length > maxLength) {
+//       confirmAlert({
+//         customUI: ({ onClose }) => (
+//           <div className={themeClass}>
+//             <p>Seu comentário deve conter entre 3 e 250 caracteres!</p>
+//             <button 
+//               className="button"
+//               onClick={() => { onClose(); setIsLoading(false) }}
+//             >
+//               Ok
+//             </button>
+//           </div>
+//         ),
+//       })
+//     } else {
+//       const commentData = { author: author, text: moderateText(text.charAt(0).toUpperCase() + text.slice(1)), authorEmail }
+//       // await handleSubmit({ author: author, text: text.charAt(0).toUpperCase() + text.slice(1), authorEmail })
+//       await handleSubmit(commentData, postId, parentId)
+//       setText()
+//       setIsLoading(false)
+//     }
+//   }
 
-  if(isLoading) return <Loading/>
+//   if(isLoading) return <Loading/>
 
-  return (
-    <form className={styles.write} onSubmit={handleNewComment}>
-      <div className={styles.inputBox}>
-        <textarea
-          value={text}
-          className={styles.input}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="escreve seu comentário..."
-        />
-        <div
-          className={`${styles.counter} ${text?.length >= maxLength ? styles.limitReached : ''}`}
-        >
-          {text?.length || 0} / {maxLength}
-        </div>
-      </div>
-      <button 
-        className="button" 
-        type="submit"
-        disabled={!text ? true : false}
-      >
-        Publicar
-      </button>
+//   return (
+//     <form className={styles.write} onSubmit={handleNewComment}>
+//       <div className={styles.inputBox}>
+//         <textarea
+//           value={text}
+//           className={styles.input}
+//           onChange={(e) => setText(e.target.value)}
+//           placeholder="escreve seu comentário..."
+//         />
+//         <div
+//           className={`${styles.counter} ${text?.length >= maxLength ? styles.limitReached : ''}`}
+//         >
+//           {text?.length || 0} / {maxLength}
+//         </div>
+//       </div>
+//       <button 
+//         className="button" 
+//         type="submit"
+//         disabled={!text ? true : false}
+//       >
+//         Publicar
+//       </button>
      
-    </form>
-  )
-}
+//     </form>
+//   )
+// }
